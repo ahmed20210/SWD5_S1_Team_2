8bc27198e36e3f4a9259ad10d3049eaf1e1df723
sharabash <ahmdmsty345@gmail.com>
2025-02-15 01:39:40 +0200

    add Ahmed Sharabash Sql Code

--- SQL.sql
+++ SQL.sql
@@ -22,8 +22,9 @@ CREATE TABLE Address (
 );
 
 ALTER TABLE [User]
-ADD CONSTRAINT FK_User_Addresss
-FOREIGN KEY (MainAddressId) REFERENCES Address(AddressId);
+ADD CONSTRAINT FK_User_Address
+FOREIGN KEY (MainAddressId) REFERENCES Address(AddressId)
+ON DELETE SET NULL;
 
 CREATE TABLE Token (
     TokenId INT PRIMARY KEY,
@@ -33,3 +34,45 @@ CREATE TABLE Token (
     ExpiryDate DATETIME,
     FOREIGN KEY (UserId) REFERENCES [User](UserId)
 );
+
+CREATE TABLE Category (
+    CategoryId INT PRIMARY KEY,
+    Name NVARCHAR(50),
+    Description NVARCHAR(50)
+    ImagePath NVARCHAR(50)
+);
+
+CREATE TABLE Product (
+    ProductId INT PRIMARY KEY,
+    CategoryId INT,
+    Name NVARCHAR(50),
+    Description NVARCHAR(50),
+    Quantity INT,
+    Status NVARCHAR(50),
+    Price DECIMAL(18, 2),
+    ViewsCount INT,
+    PurchaseCount INT,
+    ReviewsCount INT,
+    AverageReviews DECIMAL(18, 2),
+    FOREIGN KEY (CategoryId) REFERENCES Category(CategoryId)
+)
+
+CREATE Table FavouriteList (
+    UserId INT,
+    ProductId INT,
+    PRIMARY KEY (UserId, ProductId),
+    FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    FOREIGN KEY (ProductId) REFERENCES Product(ProductId)
+);
+
+CREATE TABLE Reviews(
+    ReviewId INT PRIMARY KEY,
+    UserId INT,
+    ProductId INT,
+    Rating INT,
+    Review NVARCHAR(50),
+    Date DATETIME,
+    VerifiedPurchase BIT,
+    FOREIGN KEY (UserId) REFERENCES [User](UserId),
+    FOREIGN KEY (ProductId) REFERENCES Product(ProductId)
+)
