c43d3a0582855b0bca21b47b8a93f93544ae5b50
Hekal <ahmedsanta021@gmail.com>
2025-03-03 22:31:08 +0200

    Adding Dbset for Order OrderItem Discount

--- Infrastructure/Data/EntityIndex.cs
+++ Infrastructure/Data/EntityIndex.cs
@@ -1,4 +1,5 @@
 
+using Domain;
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,4 +10,7 @@ public partial  class ApplicationDbContext
    public DbSet<Payment> Payments { get; set; }
    public DbSet<Notification> Notifications { get; set; }
    public DbSet<ReturnedOrder> ReturnedOrders { get; set; }
+   public DbSet<Order> Orders { get; set; }
+   public DbSet<OrderItem> OrderItems { get; set; }
+   public DbSet<Discount> Discounts{ get; set; }
 }
