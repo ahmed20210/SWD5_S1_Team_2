3e088944e837b67be1729b0e0de2c980cacdf944
Hekal <ahmedsanta021@gmail.com>
2025-04-28 01:51:32 +0300

    add secureSocketOptions for mailing service and the testing is done

--- Business/Services/MailingService/IMailingService.cs
+++ Business/Services/MailingService/IMailingService.cs
@@ -3,6 +3,5 @@ using Microsoft.AspNetCore.Http;
 
 public interface IMailingService
 {
-    Task SendMailAsync(string to, string subject, string body , IList<IFormFile> attachments = null);
-    
+    Task SendMailAsync(string to, string subject, string body , IList<IFormFile>? attachments = null);
 }
