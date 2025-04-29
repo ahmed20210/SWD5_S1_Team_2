01ba7ef681466e55a33e869e4c20e9b710e2c742
sharabash <ahmdmsty345@gmail.com>
2025-04-29 18:58:47 +0300

    Refactor index.html to remove carousel and product sections; add _ProductCard.cshtml for reusable product card component

--- Domain/ViewModels/ProductViewModels/ProductViewModel.cs
+++ Domain/ViewModels/ProductViewModels/ProductViewModel.cs
@@ -5,12 +5,25 @@ namespace Domain.ViewModels.ProductViewModels;
 
 public class ProductViewModel : ProductBaseViewModel
 {
-   
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+    public int? Stock { get; set; }
+    public double? AverageReviewScore { get; set; }
+    public Domain.ProductStatus? Status { get; set; }
+    public int? NoOfViews { get; set; }
+    public int? NoOfPurchase { get; set; }
+    public int? NoOfReviews { get; set; }
+    public string? ImageUrl { get; set; }
+    public int? Rating { get; set; }
+    public int? ReviewCount { get; set; }
+
 }
 
 public class CreateProductViewModel : ProductBaseViewModel
 {
-    
+
 }
 
 public class UpdateProductViewModel
@@ -27,5 +40,5 @@ public class GetAllProductsViewModel : ProductBaseViewModel
 
 public class GetProductViewModel : ProductBaseViewModel
 {
-    
+
 }
